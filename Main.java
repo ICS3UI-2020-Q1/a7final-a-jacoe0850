d917c402762e7c4a4b0c4aee3fea08b1b19c3a85
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-17 23:16:29 +0000

    Finished a7 summative

--- Main.java
+++ Main.java
@@ -1,25 +1,30 @@
 /**
- *
+ * calculates sum of 1's in an integer
  * @jacoe0850 
  */
 public class Main {
 public static int countOnes(int num){
+  // making variable for the sum of 1's
 int sum = 0;
-if(num < 0){
-  num = num*-1;
-}
+// while loop that runs whenever number is 10 or above
 while(num >= 10){
-    num = num / 10;
-  if(num == 1){
-    
+  // if the number in the while loop is 1 add 1
+  if(num % 10 == 1){
+    sum = sum + 1;
+  }
+  // removes the last digit from the integer
+  num = num / 10;
 }
-sum = sum + 1;
+// this is here because i couldn't get the while loop to work with the first digit so if the first digit is 1 it adds 1
+if(num == 1){
+  sum = sum + 1;
 }
-System.out.println(sum);
-return num % 10 + sum;
+return sum;
 }
   public static void main(String[] args) {
-    int answer = countOnes(97511531);
-    System.out.println(answer);    
+    // makes a number to insert into my method
+    int sum = countOnes(65111416);
+    // says answer
+    System.out.println(sum);    
   }
 }
